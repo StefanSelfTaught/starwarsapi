33116c79bf601ba8d2fbf49a57da39b625c5bc7c
Stefan <me@Stefan.com>
2019-05-02 20:11:22 +0300

    Updates

---
+++ precache-manifest.ac924503ce38061f4cb14106832c4703.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "460864e0b3a2c1204984",
+    "url": "/starwarsapi/static/js/runtime~main.a0f48d18.js"
+  },
+  {
+    "revision": "2d53f07b836e362bffc8",
+    "url": "/starwarsapi/static/js/main.27b6c546.chunk.js"
+  },
+  {
+    "revision": "9400034df1f06acf7245",
+    "url": "/starwarsapi/static/js/2.cba2e63e.chunk.js"
+  },
+  {
+    "revision": "2d53f07b836e362bffc8",
+    "url": "/starwarsapi/static/css/main.2f504463.chunk.css"
+  },
+  {
+    "revision": "f2f92e77bc05db08b6903649ccc17751",
+    "url": "/starwarsapi/index.html"
+  }
+];

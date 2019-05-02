bda8b3c7f35cb637d35f2111f3c430216acc4d8e
Stefan <me@Stefan.com>
2019-05-02 19:57:00 +0300

    Testing

--- src/App.js
+++ src/App.js
@@ -1,63 +1,26 @@
-import React, { Component, Fragment } from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-
-const styles = {
-  card: {
-    maxWidth: 345,
-  },
-  media: {
-    height: 140,
-  },
-};
+import React, { Component } from 'react'
+import Card from './Card';
 
 class App extends Component {
   state = {
-    data: []
+    items: ''
   }
 
   componentDidMount() {
-    const getData = fetch('https://swapi.co/api/people/1/')
+    fetch('https://swapi.co/api/people/1/')
       .then(response => response.json())
-      .then(data => console.log(data.name))
-    this.setState({ data: getData })
+      .then(data => this.setState({ items: data.name }))
   }
 
   render() {
-
-    const { classes } = this.props;
-
     return (
-      <Card className={classes.card}>
-        <CardActionArea>
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-              Lizard
-          </Typography>
-            <Typography component="p">
-              Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-              across all continents except Antarctica
-          </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary">
-            Share
-        </Button>
-          <Button size="small" color="primary">
-            Learn More
-        </Button>
-        </CardActions>
-      </Card>
+      <div>
+        <Card name={this.state.items} />
+        <Card name={this.state.items} />
+      </div>
     )
   }
 }
 
-export default App;
-
+export default App
 

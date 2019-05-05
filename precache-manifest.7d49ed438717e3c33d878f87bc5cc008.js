18aba697c347f2c4ff4c337b8147ee9cc7ea0401
Stefan <me@Stefan.com>
2019-05-05 17:27:11 +0300

    Updates

---
+++ precache-manifest.7d49ed438717e3c33d878f87bc5cc008.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "54a2b1ca006600c78a4f2dbc1f321803",
+    "url": "/starwarsapi/static/media/starWars.54a2b1ca.jpg"
+  },
+  {
+    "revision": "f72f509a2014f09c1ebf1c81294ed792",
+    "url": "/starwarsapi/static/media/Starjout.f72f509a.ttf"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/starwarsapi/static/media/Starjedi.cc3019aa.ttf"
+  },
+  {
+    "revision": "460864e0b3a2c1204984",
+    "url": "/starwarsapi/static/js/runtime~main.a0f48d18.js"
+  },
+  {
+    "revision": "ae66f59ba19969f1adc1",
+    "url": "/starwarsapi/static/js/main.1fc0288b.chunk.js"
+  },
+  {
+    "revision": "a1e13642b5da059e4697",
+    "url": "/starwarsapi/static/js/2.e6f0fd1a.chunk.js"
+  },
+  {
+    "revision": "ae66f59ba19969f1adc1",
+    "url": "/starwarsapi/static/css/main.5619b17b.chunk.css"
+  },
+  {
+    "revision": "ad1b9292fb92e979c6be791e907bdae3",
+    "url": "/starwarsapi/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/starwarsapi/precache-manifest.4dddf612410e4f90f61ded62d7a0c819.js"
+  "/starwarsapi/precache-manifest.7d49ed438717e3c33d878f87bc5cc008.js"
 );
 
 workbox.clientsClaim();
